50c9129fd6b245f14fcf6d0455a585507d6ada49
ALEXANDER <107063122+Alex-Trojans@users.noreply.github.com>
2024-03-29 16:14:43 +0530

    Update play.py

--- SoloCloud/utils/inline/play.py
+++ SoloCloud/utils/inline/play.py
@@ -54,27 +54,27 @@ def stream_markup_timer(_, chat_id, played, dur):
         bar = "—————————◉"
     buttons = [
         [
-            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
-            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
-            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
-            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
-            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
-        ],
-        [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")]
+            InlineKeyboardButton(
+             text=f"{played} {bar} {dur}",
+             callback_data="GetTimer",
+           )
+    ],
+    [
+        inlineKeyboardButton(text=_["S_B_6"],url=config.SUPPORT_CHANNEL),
+        inlineKeyboardButton(text=_["S_B_2"],url=config.SUPPORT_GROUP),
+    ],
+    [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")]
     ]
     return buttons
 
 
 def stream_markup(_, chat_id):
     buttons = [
-        [
-            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
-            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
-            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
-            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
-            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
-        ],
-        [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
+    [
+        inlineKeyboardButton(text=_["S_B_6"],url=config.SUPPORT_CHANNEL),
+        inlineKeyboardButton(text=_["S_B_2"],url=config.SUPPORT_GROUP),
+    ],
+    [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
     return buttons
 
